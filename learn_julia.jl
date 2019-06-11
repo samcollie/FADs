916751c9ebdae7e2772d314ca98ec8e0f79a7f24
samcollie <scollie@ucsb.edu>
2019-06-11 07:43:39 -0700

    Staring to learn julia!

---
+++ learn_julia.jl
@@ -0,0 +1,20 @@
+# Goal: Find the optimal number and placement of FADs in a simple fishing ground
+#       This is the "simplest possible" model.
+
+# Step 1: Initialize a model fishing ground
+carrying_capacity = 100
+dim = 2     # The number of patches on one edge of the [square] fishing ground
+npatches = dim^2    # Total number of patches
+
+# Initialize the stock + harvest at 1/2 & 1/4 of the state space respectively
+stock = carrying_capacity/2*ones(npatches)         # The fish stock
+harvest = carrying_capacity/4*ones(npatches)       # Harvest
+
+# An infinitely migratory dispersial matrix
+dispersial = (1/npatches)*ones(npatches, npatches)
+
+# Given the current stock & harvest, return the stock in the next period
+# This is a Gordon-Schaefer Logistic Growth Model
+function stock_next(s,h)
+    stock_next = s + 0.8*s*(1-s/carrying_capacity) - h
+end
